c54bb66962c2d0a8db165de68a1f6cc988621b4f
Gopikrishna-M-A <gopikrishna6003@gmail.com>
2021-06-09 16:59:40 +0530

    third

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+var numId = 0
+
 function addButtonClicked() {
     var text
+    
    
     text = document.getElementById('addBox').value
     var parent = document.getElementById('mainFrame');
-    var newChild = ' <input class="displaybox" type="text" disabled value="'+text+'">';
+    var newChild = '<div id='+numId+' > <input class="displaybox" type="text" disabled value="'+text+'">  <span onclick="deleteButtonClicked('+numId+')" class="del"><i class="material-icons">delete</i></span>  </div>';
     parent.insertAdjacentHTML('beforeend', newChild);
     document.getElementById('addBox').value= ''
+    numId++
    
     
+}
+
+function deleteButtonClicked(number)
+{
+    var obj = document.getElementById(number)
+    obj.remove()
+    
+   
+  
 }
